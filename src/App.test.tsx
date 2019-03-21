5b56a7e8858aef6d032aa48d830e4cf9a104bc83
castaneai <castaneai@castaneai.net>
2019-03-21 19:12:35 +0900

    Pass test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { readFileSync } from "fs";
+firebase.initializeApp(
+  JSON.parse(readFileSync("public/firebase-config.json", "utf-8"))
+);
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
